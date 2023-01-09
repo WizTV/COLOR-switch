ae2b9c2314660e27bee1fdc4158b43b4b9619222
WizTV <pawel.wiezik@gmail.com>
2023-01-09 21:45:42 +0100

    toggle dark mode

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("dark").addEventListener("click", () => {
+	document.getElementById("body").classList.toggle("dark-bg");
+	document.getElementById("h1").classList.toggle("dark-txt");
+	document.getElementById("p").classList.toggle("dark-txt");
+});
